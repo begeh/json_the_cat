0e33503aed6b1522a9db5c2f9030d237e39ea0f2
Bashir Egeh <b_egeh@hotmail.com>
2020-01-16 19:25:23 +0000

    edits

--- breedfetcher.js
+++ breedfetcher.js
@@ -1,14 +1,19 @@
 const request = require('request');
 let args = process.argv.slice(2);
 let breed = args[0];
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    return console.log('Request failed');
-  }
-  const data = JSON.parse(body);
-  if (response.statusCode === 200 && data.length === 1) {
-    return console.log(data[0].description);
-  } else {
-    return console.log("Invalid breed name");
-  }
-});
+
+const fetchBreedDescription = function (breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    if (error) {
+      return console.log('Request failed');
+    }
+    const data = JSON.parse(body);
+    if (response.statusCode === 200 && data.length === 1) {
+      return console.log(data[0].description);
+    } else {
+      return console.log("Invalid breed name");
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
